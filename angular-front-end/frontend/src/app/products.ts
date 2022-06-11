0532c7b9a5accb528b9b0f25bb105d53808b71e8
merchuey <mercedezhueyopa@gmail.com>
2022-06-11 09:39:01 -0700

    front

---
+++ angular-front-end/frontend/src/app/products.ts
@@ -0,0 +1,23 @@
+export interface Product{
+    SKU: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products = [
+    {
+        SKU: 1,
+        name: 'Anime fig. test',
+        price: 100,
+        description: 'best on the market. worth the price'
+    },
+    {
+        SKU: 2,
+        name: 'Anime fig test2',
+        price:200,
+        description: 'worth every penny'
+    },
+
+    //find out if this needs to be "zipped up"
+]
