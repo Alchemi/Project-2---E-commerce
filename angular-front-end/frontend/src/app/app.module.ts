b299e9595eb15146ef9e620f63a308e071b568c8
merchuey <mercedezhueyopa@gmail.com>
2022-06-11 22:28:35 -0700

    added cart compos, but having trouble w it

--- angular-front-end/frontend/src/app/app.module.ts
+++ angular-front-end/frontend/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+import{ TopBarComponent } from './top-bar/top-bar.component';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    TopBarComponent,
     ProductDetailsComponent,
     ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +26,7 @@ import { ProductListComponent } from './product-list/product-list.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component:
     ProductDetailsComponent },
+    { path: 'cart', component: CartComponent },
     ])
   ],
   providers: [],
--- angular-front-end/frontend/src/app/product-details/product-details.component.ts
+++ angular-front-end/frontend/src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
     selector: 'app-product-details',
@@ -10,9 +11,18 @@ import { Product, products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
+    addToCart(product: Product) {
+        this.cartService.addToCart(product);
+        window.alert("you have added this item to your shopping cart!");
+    
+    }
+
     product: Product | undefined;
     
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private cartService: CartService
+        ) { }
     ngOnInit(): void {
     //get the product id from the current route
     const routeParams = this.route.snapshot.paramMap;
