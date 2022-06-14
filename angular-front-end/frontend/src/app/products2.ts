bc27313f41e3d8e6c9a9394523e5f8f4efd0ec37
merchuey <mercedezhueyopa@gmail.com>
2022-06-14 15:23:19 -0700

    added 2 more products. manga and videos

---
+++ angular-front-end/frontend/src/app/products2.ts
@@ -0,0 +1,43 @@
+export interface Product2{
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products2 =[
+   {
+    id: 2.1,
+    name: "manga 1",
+    price: 9.99,
+    description: "this one is good"
+   },
+
+   {
+    id: 2.2,
+    name: "manga 2",
+    price: 9.99,
+    description: "this one is better"
+   },
+
+   {
+    id: 2.3,
+    name: "manga 3",
+    price: 9.99,
+    description: "this one is alright"
+   },
+
+   {
+    id: 2.4,
+    name: "manga 4",
+    price: 9.99,
+    description: "this one is not that good"
+   },
+
+   {
+    id: 2.5,
+    name: "manga 5",
+    price: 9.99,
+    description: "this one is not that bad"
+   },
+];
