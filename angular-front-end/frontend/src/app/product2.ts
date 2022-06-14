147da64637926b54c07b44464ccba998f666095a
merchuey <mercedezhueyopa@gmail.com>
2022-06-13 20:08:53 -0700

    added product2 and product3 ts files

---
+++ angular-front-end/frontend/src/app/product2.ts
@@ -0,0 +1,22 @@
+export interface product2{
+    Id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products2 = [
+    {
+        Id: 2.1,
+        name: 'Manga test 1',
+        price: 100,
+        description: 'this one is good'
+    },
+    {
+        Id: 2.2,
+        name: 'Managa test 2',
+        price:200,
+        description: 'this one is better'
+    },
+    //find out if this needs to be "zipped up" - sparkey thoughts
+]
---
+++ angular-front-end/frontend/src/app/product3.ts
@@ -0,0 +1,23 @@
+export interface Product3{
+    Id: number;
+    name: string;
+    price: number;
+    description: string;
+}
+
+export const products = [
+    {
+        Id: 3.1,
+        name: 'Vid 1',
+        price: 100,
+        description: 'the manga was better'
+    },
+    {
+        Id: 2,
+        name: 'Vid 2',
+        price:200,
+        description: 'the movie/series was better'
+    },
+
+    //find out if this needs to be "zipped up" - sparkey thoughts
+]
