b731aee1b95bcb22cc3cbb132f174bebc29295bc
ah836798 <arlontus@hotmail.com>
2022-06-17 21:30:53 -0500

    added login

--- angular-front-end/frontend/src/app/auth/components/login/login.component.ts
+++ angular-front-end/frontend/src/app/auth/components/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { ProgressBarService } from 'src/app/shared/services/progress-bar.service';
-import { AlertService } from 'ngx-alerts';
+import { AlertService } from 'ngx-alerts'
+
 
 @Component({
   selector: 'app-login',
